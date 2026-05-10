ce82411f0f8735e0ccfda583d25b70d5e10dca3c
Dewran-9 <dewran.9@hotmail.com>
2026-05-10 23:49:28 +0200

    US2 - tilføjet settings model, controller og seed med globale toggles for tts, bip, ff, leaderboard og visuel feedback

---
+++ src/Fartmaaler/FartmaalerAPI/Models/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace FartmaalerAPI.Models
+{
+    public class Settings
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public bool Value { get; set; }
+    }
+}
